90ebff142ba409e72ad393842fa5a98555c4490c
SNIFF <ernestceo@hotmail.com>
2022-03-17 20:23:13 -0400

    additional colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'ori-green-light':'#538d4e',
+        'ori-green-dark':'#325b2f',
+        'ori-green-darkest':'#2a4c28',
+        'ori-yellow-light':'#b59f3b',
+        'ori-yellow-dark':'#827021',
+        'ori-yellow-darkest':'#6a5b17',
+        'darkBackground':'#121212',
+        'hc-red-light':'#bd4747',
+      }
+    },
   },
   plugins: [],
 }
