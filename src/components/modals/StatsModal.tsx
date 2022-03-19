fccc01a0202f71b1833c42e8027085c91f724528
SNlFF <ernestceo@hotmail.com>
2022-03-19 18:47:42 -0400

    focus delete

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -57,7 +57,7 @@ export const StatsModal = ({
       handleClose={handleClose}
     >
       <StatBar gameStats={gameStats} />
-      <h4 className="text-lg leading-6 font-medium text-gray-900 dark:text-gray-100">
+      <h4 className="text-lg leading-6 font-medium text-neutral-900 dark:text-neutral-100">
         {GUESS_DISTRIBUTION_TEXT}
       </h4>
       <Histogram
@@ -69,14 +69,14 @@ export const StatsModal = ({
           <div>
             <h5>{NEW_WORD_TEXT}</h5>
             <Countdown
-              className="text-lg font-medium text-gray-900 dark:text-gray-100"
+              className="text-lg font-medium text-neutral-900 dark:text-neutral-100"
               date={tomorrow}
               daysInHours={true}
             />
           </div>
           <button
             type="button"
-            className="mt-2 w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+            className="mt-2 w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-hc-red-light text-base font-medium text-white hover:bg-hc-red-dark focus:outline-none focus:ring-1 focus:ring-offset-2 focus:ring-neutral-400 sm:text-sm"
             onClick={() => {
               shareStatus(
                 guesses,
